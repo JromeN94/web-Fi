6ab3810c65b3b1f1ce57713302fc5dcc20a18d97
JromeN94 <96178823+JromeN94@users.noreply.github.com>
2021-12-16 03:41:59 +0700

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar clipped-left fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+
+      <v-spacer />
+
+      <v-menu bottom left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.to">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="black lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-account-star",
+          title: "ข้อมูลผู้ประกอบการ",
+          to: "/Entrepreneur",
+        },
+        {
+          icon: "mdi-account-star",
+          title: "ผู้เชี่ยวชาญ",
+          to: "/Expert",
+        },
+        {
+          icon: "mdi-account-star",
+          title: "สินค้าและบริการ",
+          to: "/Product",
+        },
+        {
+          icon: "mdi-account-star",
+          title: "หน่วยงานที่ให้บริการ",
+          to: "/Network",
+        },
+        {
+          icon: "mdi-account-star",
+          title: "เข้าสู่ระบบ",
+          to: "/login",
+        },
+        {
+          icon: "mdi-account-star",
+          title: "Admin",
+          to: "/admin",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Food Innopolis",
+    };
+  },
+};
+</script>
